371a6266663a291ec4ca57c8b05755f05e00e5bf
Aman Setia <amansetiarjp@gmail.com>
2020-03-26 22:56:20 +0530

    Added chat-log chat-message chat-composer

---
+++ resources/assets/js/components/ChatMessage.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div>
+    <p>{{ message }}</p>
+    <small>{{ user }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "This is sometext",
+      user: "USER"
+    };
+  }
+};
+</script>
+
+<style lang="css"></style>
